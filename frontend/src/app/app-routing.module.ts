e0e850d4073e414cc53f4a810fc3eaefdf82c73c
Thaíssa Durães <thaissaduraes67@gmail.com>
2022-07-26 21:22:39 -0300

    atualzações vulnerabilites

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 const routes: Routes = [
   {
